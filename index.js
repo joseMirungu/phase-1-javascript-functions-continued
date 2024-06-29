9e548a3e40e97867450fbe4ce98dbd46d20340a9
joseMirungu <josephirungu890@gmail.com>
2024-06-29 23:08:23 +0300

    update

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+function saturdayFun(defaultActivity=`roller-skate`){
+    return `This Saturday, I want to ${defaultActivity}!`
+}
+function mondayWork(activity = "go to the office"){
+    return `This Monday, I will ${activity}.`
+}
+function wrapAdjective(highlight = `*`){
+    return function(highlight2 = "||"){
+    return `You are ${highlight}${highlight2}${highlight}!`;
+   }
+}
